37e19a6c43b1ff370edb67028e4ce745ce7c069d
faaus <faaus@DESKTOP-GDPNHA9>
2022-11-15 18:42:46 -0300

    Agregar archivos de proyecto.

---
+++ FrmTriatlon.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace PrySp4Triatlon_Cantallops
+{
+    partial class FrmTriatlon
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAsignar = new System.Windows.Forms.Button();
+            this.btnDeterminar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.picGanadores = new System.Windows.Forms.PictureBox();
+            this.dgvParticipantes = new System.Windows.Forms.DataGridView();
+            this.Participante6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Participante5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Participante4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Participante3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Participante2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Participante1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prueba = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvPosiciones = new System.Windows.Forms.DataGridView();
+            this.Puntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pais1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Puesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvGanadores = new System.Windows.Forms.DataGridView();
+            this.Numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.País = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanadores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParticipantes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPosiciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGanadores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAsignar
+            // 
+            this.btnAsignar.Location = new System.Drawing.Point(814, 195);
+            this.btnAsignar.Name = "btnAsignar";
+            this.btnAsignar.Size = new System.Drawing.Size(104, 37);
+            this.btnAsignar.TabIndex = 4;
+            this.btnAsignar.Text = "Asignar Participantes";
+            this.btnAsignar.UseVisualStyleBackColor = true;
+            this.btnAsignar.Click += new System.EventHandler(this.btnAsignar_Click);
+            // 
+            // btnDeterminar
+            // 
+            this.btnDeterminar.Location = new System.Drawing.Point(814, 248);
+            this.btnDeterminar.Name = "btnDeterminar";
+            this.btnDeterminar.Size = new System.Drawing.Size(104, 38);
+            this.btnDeterminar.TabIndex = 5;
+            this.btnDeterminar.Text = "Determinar Ganadores";
+            this.btnDeterminar.UseVisualStyleBackColor = true;
+            this.btnDeterminar.Click += new System.EventHandler(this.btnDeterminar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(814, 310);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(104, 35);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // picGanadores
+            // 
+            this.picGanadores.Location = new System.Drawing.Point(634, 411);
+            this.picGanadores.Name = "picGanadores";
+            this.picGanadores.Size = new System.Drawing.Size(100, 50);
+            this.picGanadores.TabIndex = 7;
+            this.picGanadores.TabStop = false;
+            // 
+            // dgvParticipantes
+            // 
+            this.dgvParticipantes.AllowUserToAddRows = false;
+            this.dgvParticipantes.AllowUserToDeleteRows = false;
+            this.dgvParticipantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParticipantes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Numero,
+            this.Nombre,
+            this.País});
+            this.dgvParticipantes.Location = new System.Drawing.Point(12, 27);
+            this.dgvParticipantes.Name = "dgvParticipantes";
+            this.dgvParticipantes.Size = new System.Drawing.Size(594, 160);
+            this.dgvParticipantes.TabIndex = 0;
+            this.dgvParticipantes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Participante6
+            // 
+            this.Participante6.HeaderText = "Participante 6";
+            this.Participante6.Name = "Participante6";
+            // 
+            // Participante5
+            // 
+            this.Participante5.HeaderText = "Participante 5";
+            this.Participante5.Name = "Participante5";
+            // 
+            // Participante4
+            // 
+            this.Participante4.HeaderText = "Participante 4";
+            this.Participante4.Name = "Participante4";
+            // 
+            // Participante3
+            // 
+            this.Participante3.HeaderText = "Participante 3";
+            this.Participante3.Name = "Participante3";
+            // 
+            // Participante2
+            // 
+            this.Participante2.HeaderText = "Participante 2";
+            this.Participante2.Name = "Participante2";
+            // 
+            // Participante1
+            // 
+            this.Participante1.HeaderText = "Participante 1";
+            this.Participante1.Name = "Participante1";
+            // 
+            // Prueba
+            // 
+            this.Prueba.HeaderText = "Prueba";
+            this.Prueba.Name = "Prueba";
+            // 
+            // dgvPosiciones
+            // 
+            this.dgvPosiciones.AllowUserToAddRows = false;
+            this.dgvPosiciones.AllowUserToDeleteRows = false;
+            this.dgvPosiciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPosiciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Prueba,
+            this.Participante1,
+            this.Participante2,
+            this.Participante3,
+            this.Participante4,
+            this.Participante5,
+            this.Participante6});
+            this.dgvPosiciones.Location = new System.Drawing.Point(12, 242);
+            this.dgvPosiciones.Name = "dgvPosiciones";
+            this.dgvPosiciones.Size = new System.Drawing.Size(745, 94);
+            this.dgvPosiciones.TabIndex = 1;
+            // 
+            // Puntos
+            // 
+            this.Puntos.HeaderText = "Puntos";
+            this.Puntos.Name = "Puntos";
+            // 
+            // Pais1
+            // 
+            this.Pais1.HeaderText = "País";
+            this.Pais1.Name = "Pais1";
+            // 
+            // Nombre1
+            // 
+            this.Nombre1.HeaderText = "Nombre";
+            this.Nombre1.Name = "Nombre1";
+            // 
+            // Puesto
+            // 
+            this.Puesto.HeaderText = "Puesto";
+            this.Puesto.Name = "Puesto";
+            // 
+            // dgvGanadores
+            // 
+            this.dgvGanadores.AllowUserToAddRows = false;
+            this.dgvGanadores.AllowUserToDeleteRows = false;
+            this.dgvGanadores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGanadores.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Puesto,
+            this.Nombre1,
+            this.Pais1,
+            this.Puntos});
+            this.dgvGanadores.Location = new System.Drawing.Point(12, 363);
+            this.dgvGanadores.Name = "dgvGanadores";
+            this.dgvGanadores.Size = new System.Drawing.Size(443, 98);
+            this.dgvGanadores.TabIndex = 2;
+            // 
+            // Numero
+            // 
+            this.Numero.HeaderText = "Numero";
+            this.Numero.Name = "Numero";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 250;
+            // 
+            // País
+            // 
+            this.País.HeaderText = "País";
+            this.País.Name = "País";
+            this.País.Width = 200;
+            // 
+            // FrmTriatlon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 575);
+            this.Controls.Add(this.picGanadores);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnDeterminar);
+            this.Controls.Add(this.btnAsignar);
+            this.Controls.Add(this.dgvGanadores);
+            this.Controls.Add(this.dgvPosiciones);
+            this.Controls.Add(this.dgvParticipantes);
+            this.Name = "FrmTriatlon";
+            this.Text = "Triatlon";
+            this.Load += new System.EventHandler(this.FrmTriatlon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picGanadores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParticipantes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPosiciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGanadores)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAsignar;
+        private System.Windows.Forms.Button btnDeterminar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox picGanadores;
+        private System.Windows.Forms.DataGridView dgvParticipantes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Participante1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prueba;
+        private System.Windows.Forms.DataGridView dgvPosiciones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Puntos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pais1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Puesto;
+        private System.Windows.Forms.DataGridView dgvGanadores;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn País;
+    }
+}
+
---
+++ FrmTriatlon.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrySp4Triatlon_Cantallops
+{
+    public partial class FrmTriatlon : Form
+    {
+        public struct DATOS
+        {
+            public int participante;
+            public int puntos;
+        }
+
+        public FrmTriatlon()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FrmTriatlon_Load(object sender, EventArgs e)
+        {
+            // inicializar formulario
+            int i;
+            // agregar 6 filas a la grilla de participantes
+            for (i = 1; i <= 6; i++)
+            {
+                //asignar en cada fila solo el numero de participante (i)
+                dgvParticipantes.Rows.Add(i.ToString());
+            }
+            // configurar la primera columna que sea de solo lectura
+            dgvParticipantes.Columns[0].ReadOnly = true;
+            //deshabilitar la grilla de posiciones
+            dgvPosiciones.Enabled = false;
+        }
+
+        private void btnAsignar_Click(object sender, EventArgs e)
+        {
+            int i;
+            dgvPosiciones.Rows.Clear();
+            // verificar que esten cargados todos los participantes
+            for (i = 0;  i < 6; i++)
+            {
+                if (dgvParticipantes.Rows[i].Cells[1].Value == null ||
+                    dgvParticipantes.Rows[i].Cells[2].Value == null)
+                {
+                    MessageBox.Show("Debe completar los datos del participante",
+                        "ERROR", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+            //configurar las columnas de la grilla
+            for (i = 0; i < 6; i++)
+            {
+                dgvPosiciones.Columns[i + 1].HeaderText =
+                    dgvParticipantes.Rows[i].Cells[1].Value.ToString();
+            }
+            //configurar las filas con las 3 pruebas del Triatlon
+            for (i = 0; i < 3; i++)
+            {
+                dgvPosiciones.Rows.Add();
+            }
+            dgvPosiciones.Rows[0].Cells[0].Value = "Natación";
+            dgvPosiciones.Rows[1].Cells[0].Value = "Ciclismo";
+            dgvPosiciones.Rows[2].Cells[0].Value = "Carrera";
+            dgvPosiciones.Columns[0].ReadOnly = true;
+            dgvParticipantes.ClearSelection();
+            dgvPosiciones.Enabled = true;
+
+        }
+
+        private void btnDeterminar_Click(object sender, EventArgs e)
+        {
+            int fila;
+            int col;
+            int i;
+            string valor;
+            string nombre;
+            string nacionalidad;
+
+            //controlar los datos ingresados en la grilla de posiciones
+            if (!ValidarPosiciones())
+            {
+                MessageBox.Show("Los datos son incorrectos, revise las posiciones asignadas",
+                    "ERROR", MessageBoxButtons.OK);
+                return;
+            }
+            dgvPosiciones.ClearSelection();
+
+            int[,] puestos = new int[3, 6]; // arreglo bidemnsional
+            // copiar el contenido de la grilla al arreglo bidimensional
+            for (fila = 0; fila < 3; fila++)
+            {
+                for (col = 0; col < 6; col++)
+                {
+                    // obtener el valor de cada celda
+                    valor = dgvPosiciones.Rows[fila].Cells[col + 1].Value.ToString();
+                    puestos[fila, col] = int.Parse(valor); // convertirlo a entero
+                }
+            }
+            DATOS[] datos = new DATOS[6];
+            for (i = 0; i < 6; i++)
+            {
+                datos[i].participante = i + 1; // numero de participante
+                datos[i].puntos = 0; // cantidad de puntos del participante
+            }
+            // recorrer el arreglo bidimensional de los puestos y sumar los puntos
+            for (col = 0; col < 6; col++)
+            {
+                for (fila = 0; fila < 3; fila++)
+                {
+                    datos[col].puntos += ObtenerPuntos(puestos[fila, col]);
+                }
+            }
+
+            //arreglo de tipo DATOS para los ganadores
+            DATOS[] ganadores = new DATOS[3];
+            ganadores[0] = BuscarMayorPuntaje(datos, ganadores);
+            ganadores[1] = BuscarMayorPuntaje(datos, ganadores);
+            ganadores[2] = BuscarMayorPuntaje(datos, ganadores);
+
+            //cargar la grilla de ganadores
+            dgvGanadores.Rows.Clear();
+            for (fila = 0; fila < 3; fila++)
+            {
+                dgvGanadores.Rows.Add(); // se agrega una nueva fila
+                dgvGanadores.Rows[fila].Cells[0].Value = fila + 1; // posicion final
+                // obtener la fial que ocupa el participante en la grilla 1
+                int f = ganadores[fila].participante - 1;
+                // obtener el nombre del participante
+                nombre = dgvParticipantes.Rows[f].Cells[1].Value.ToString();
+                dgvGanadores.Rows[fila].Cells[1].Value = nombre;
+                // obtener la nacionalidad del participante
+                nacionalidad = dgvParticipantes.Rows[f].Cells[2].Value.ToString();
+                dgvGanadores.Rows[fila].Cells[2].Value = nacionalidad;
+                // puntos obtenidos por el participante
+                dgvGanadores.Rows[fila].Cells[3].Value = ganadores[fila].puntos.ToString();
+            }
+            dgvGanadores.ClearSelection();
+
+            
+        }
+
+        private bool ValidarPosiciones()
+        {
+            int f;
+            int c;
+            int i;
+            int valor;
+            bool resultado = true;
+            for (f = 0; f < dgvPosiciones.RowCount; f++)
+            {
+                for(c = 1; c < dgvPosiciones.ColumnCount; c++)
+                {
+                    valor = int.Parse(dgvPosiciones.Rows[f].Cells[c].Value.ToString());
+                    if (valor < 1 || valor > 6)
+                    {
+                        resultado = false;
+                        break;
+                    }
+                    //verificar que el valor de esa celda no este repetido
+                    // en las siguientes posiciones de la fila
+                    for(i = c + 1; i < dgvPosiciones.ColumnCount; i++)
+                    {
+                        if (valor == int.Parse(dgvPosiciones.Rows[f].Cells[i].Value.ToString()))
+                        {
+                            resultado = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            return resultado;
+        }
+
+
+        private int ObtenerPuntos(int puesto)
+        {
+            // arreglo con los puntos por el puesto en cada prueba:
+            // puesto 1: 1000 puntos
+            // puesto 2: 920 puntos
+            // puesto 3: 830 puntos
+            // puesto 4: 740 puntos
+            // puesto 5: 650 puntos
+            // puesto 6: 560 puntos
+            int[] puntos = new int[6] { 1000, 920, 830, 740, 650, 560 };
+            //sed dfevuelve el valor del elemento puesto -1
+            return puntos[puesto - 1];
+        }
+
+
+        private DATOS BuscarMayorPuntaje(DATOS[] d, DATOS[] g)
+        {
+            DATOS resultado;
+            int i;
+            int f;
+            int ganadorPrevio;
+
+            resultado.participante = 0;
+            resultado.puntos = 0;
+
+            for (i = 0; i < 6; i++)
+            {
+                if (d[i].puntos > resultado.puntos)
+                {
+                    ganadorPrevio = 0;
+                    for (f= 0; f < g.Length; f++)
+                    {
+                        if (d[i].participante == g[f].participante)
+                        {
+                            ganadorPrevio = 1;
+                        }
+                    }
+                    if (ganadorPrevio == 0)
+                    {
+                        resultado.puntos = d[i].puntos;
+                        resultado.participante = d[i].participante;
+                    }
+                }
+            }
+            return resultado;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
